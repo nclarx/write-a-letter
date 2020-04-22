69ef6526f78138034d684a4e9e504fc4e4bcff4e
Nick Clark <nick@clarx.dev>
2020-04-22 20:00:38 +1000

    Added some comments and a useEffect hook to ImageMetaList component

--- src/components/ImageMetaList.tsx
+++ src/components/ImageMetaList.tsx
@@ -1,7 +1,7 @@
-import { observer }  from 'mobx-react'
-import React         from 'react'
-import { ImageMeta } from '../models/imageMeta'
-import Postcard      from './Postcard'
+import { observer }         from 'mobx-react'
+import React, { useEffect } from 'react'
+import { ImageMeta }        from '../models/imageMeta'
+import Postcard             from './Postcard'
 
 interface ImageMetaListProps {
     imageMeta: ImageMeta[]
@@ -10,6 +10,12 @@ interface ImageMetaListProps {
 const ImageMetaList = observer((props: ImageMetaListProps): JSX.Element => {
 
     const imageMeta = props.imageMeta
+
+    useEffect(() => {
+        console.log('Image Meta updated, number of imageMeta: ', props.imageMeta.length)
+        console.log(`Look at me, i'm not just an array of image meta..i'm wrapped in MobX's observable stuff: `, props.imageMeta)
+        console.log(`This use effect fires because its dependency is imageMeta.length`)
+    }, [props.imageMeta.length])
 
     const renderCardList = () => (
         <section>
